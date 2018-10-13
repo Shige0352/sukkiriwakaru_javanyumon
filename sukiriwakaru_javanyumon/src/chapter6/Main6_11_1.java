8c980a1c97c43293d068d3a02c043aadb15e46d7
Shige0352 <shigeaki52@gmail.com>
2018-10-13 10:23:10 +0900

    第6章終わり

---
+++ sukiriwakaru_javanyumon/src/chapter6/Main6_11_1.java
@@ -0,0 +1,12 @@
+package chapter6;
+
+public class Main6_11_1 {
+	public static void main(String[] args) throws Exception {
+		Main6_11_1_1.doWarusa();
+		Main6_11_1_1.doTogame();
+		Main6_11_1_2.callDeae();
+		Main6_11_1_2.showMondokoro();
+
+	}
+
+}
